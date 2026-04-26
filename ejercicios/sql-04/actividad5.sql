f2c5ab20f5b05f15b757c008417293f4578ecfd5
Nicolas R <regueira2010@gmail.com>
2026-04-26 17:31:24 -0400

    ejercicio: finalizar análisis de catálogo Netflix con operadores LIKE, BETWEEN e IN

--- ejercicios/sql-04/actividad5.sql
+++ ejercicios/sql-04/actividad5.sql
@@ -8,6 +8,66 @@ CREATE TABLE serie_netflix
     PRIMARY KEY (nombre)
 );
 
-insert into serie_netflix (nombre, temporadas, genero, anio_estreno) values ('Black Mirror', 5, 'Ciencia ficci髇', 2011);
+INSERT INTO serie_netflix (nombre, temporadas, genero, anio_estreno) VALUES 
+('Black Mirror', 5, 'Ciencia ficci贸n', 2011);
 
-select * from serie_netflix;
+SELECT * FROM serie_netflix 
+
+INSERT INTO serie_netflix (nombre, temporadas, genero, anio_estreno) VALUES 
+('The Bear', 2, 'Drama', 2022),
+('The Mandalorian', 3, 'Ciencia ficci贸n', 2007),
+('Ted Lasso', 3, 'Comedia', 2020),
+('Severance', 1, 'Ciencia ficci贸n', 2022),
+('The White Lotus', 2, 'S谩tira', 2021),
+('Yellowstone', 5, 'Drama', 2008),
+('Euphoria', 2, 'Drama', 2009),
+('Lupin', 3, 'Crimen', 2021),
+('Beef', 7, 'Comedia Drama', 2023);
+
+SELECT * FROM serie_netflix 
+WHERE temporadas > 3 ORDER BY anio_estreno DESC;
+
+SELECT MIN(anio_estreno) AS anio_antiguo 
+FROM serie_netflix;
+
+SELECT MAX(anio_estreno) AS anio_nuevo 
+FROM serie_netflix;
+
+SELECT ROUND(AVG(anio_estreno)) 
+AS anio_promedio FROM serie_netflix 
+
+SELECT ROUND(AVG(temporadas)) 
+AS temporada_promedio FROM serie_netflix 
+
+SELECT * FROM serie_netflix
+WHERE temporadas IN (1, 2, 4, 5, 7)
+ORDER BY temporadas ASC;
+
+SELECT * FROM serie_netflix
+WHERE temporadas NOT IN (1, 2, 4, 5, 7)
+ORDER BY temporadas ASC;
+
+DELETE FROM serie_netflix WHERE anio_estreno > 2010;
+
+INSERT INTO serie_netflix (nombre, temporadas, genero, anio_estreno) VALUES 
+('Black Mirror', 5, 'Ciencia ficci贸n', 2011),
+('The Bear', 2, 'Drama', 2022),
+('Ted Lasso', 3, 'Comedia', 2020),
+('Severance', 1, 'Ciencia ficci贸n', 2022),
+('The White Lotus', 2, 'S谩tira', 2021),
+('Lupin', 3, 'Crimen', 2021),
+('Beef', 7, 'Comedia Drama', 2023);
+
+INSERT INTO serie_netflix (nombre, temporadas, genero, anio_estreno) 
+VALUES ('Doctor House', 8, 'Drama M茅dico', 2004);
+
+SELECT * FROM serie_netflix
+WHERE anio_estreno BETWEEN 2005 AND 2020
+ORDER BY anio_estreno ASC;
+
+SELECT * FROM serie_netflix
+WHERE nombre LIKE 'b%' OR nombre LIKE '%e'
+
+SELECT nombre, (anio_estreno + temporadas) AS calculo 
+FROM serie_netflix WHERE (anio_estreno + temporadas) > 2010;
+
